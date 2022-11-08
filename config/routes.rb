88bf1651bd15157ef0f2c7f5a4dcfc7349f77821
kevin <kevin@soucheyre.org>
2022-11-08 17:44:35 +0100

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :attendances
   resources :events
   devise_for :users
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
